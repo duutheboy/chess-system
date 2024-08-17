0ee18cc9c5a73bd8f46fc7d0bc29260fb0956585
José Eduardo <eduardobezerra777@outlook.com>
2024-08-16 22:44:31 -0300

    feat: chess layer and priting the board

--- java-chess-system/src/application/Program.java
+++ java-chess-system/src/application/Program.java
@@ -1,11 +1,11 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Position pos = new Position(9,9);
-
-        System.out.println(pos);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
