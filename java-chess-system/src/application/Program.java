a0b2f7f50c20c6849e8a1b77508fe8d3a261aefe
José Eduardo <eduardobezerra777@outlook.com>
2024-08-16 21:35:27 -0300

    feat: first class Position

--- java-chess-system/src/application/Program.java
+++ java-chess-system/src/application/Program.java
@@ -1,4 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(9,9);
+
+        System.out.println(pos);
+    }
 }
